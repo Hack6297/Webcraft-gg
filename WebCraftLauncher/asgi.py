2c7ea8b6258db0972324c1d5aa873a98296caee6
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2025-05-23 23:19:39 -0700

    Update asgi.py

--- WebCraftLauncher/asgi.py
+++ WebCraftLauncher/asgi.py
@@ -2,7 +2,7 @@ import os
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WebCraftLauncher.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WebCraft.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
