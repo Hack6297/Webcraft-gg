d100f870b1c22a1e4bf029521d9fe18d931daa49
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2025-05-24 21:55:02 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == "__main__":
-
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webcraft_launcher_site.settings')
-
+def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebCraftLauncher.settings')
-
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webcraft_launcher_site.settings')
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError("Couldn't import Django.") from exc
+        raise ImportError(
+            "Couldn't import Django. Make sure it's installed and "
+            "available on your PYTHONPATH environment variable."
+        ) from exc
     execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
